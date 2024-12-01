130bbef9f2f2f66553929338ca1a16886db51fd1
Jim lloyd <jimlloyddelavirgen00.com>
2024-12-01 19:28:44 +0800

    Initial push

--- crochetConnect/src/app/app-routing.module.ts
+++ crochetConnect/src/app/app-routing.module.ts
@@ -4,13 +4,20 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+    redirectTo: 'blank',
+    pathMatch: 'full', // Redirect to 'blank' on the base path
+  },
+  {
+    path: 'blank',
+    loadChildren: () =>
+      import('./blank/blank.module').then((m) => m.BlankPageModule),
+  },
 ];
+
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
